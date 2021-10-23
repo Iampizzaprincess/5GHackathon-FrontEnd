a94aa648a4e3286779d66f66f1cdb5ce833cc59c
msrasheed <moizr95@gmail.com>
2021-10-23 01:41:04 -0400

    dummy list items

--- src/App.js
+++ src/App.js
@@ -2,22 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BetList />
+  );
+}
+
+function BetList() {
+  let bets = [];
+  for (let i = 0; i < 4; i++) {
+    bets.push(<BetListItem />)
+  }
+  return (
+    <div>
+      {bets}
+    </div>
+  );
+}
+
+function BetListItem() {
+  return (
+    <div>
+      <div>BetListing</div>
+      <div>Like</div>
+      <div>Dislike</div>
     </div>
   );
 }
