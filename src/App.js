8e7f42ed034b6ba03cbb91b31d89c518ee4419b5
Iampizzaprincess <jacmalayter@gmail.com>
2021-10-23 17:13:30 -0400

    form take vals, puts in state.

--- src/App.js
+++ src/App.js
@@ -31,17 +31,33 @@ function App() {
 class WriteBet extends React.Component {
   constructor(props){
     super(props);
-      this.state = {value: ''};
+      this.state = {value: '', 
+                    opt1:  '',
+                    opt2:  '',
+                    min:   ''
+    };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange(event, num) {
+    //this.setState({value: event.target.value});
+    switch (num){
+      case 1: this.setState({value: event.target.value});
+            break;
+      case 2: this.setState({opt1: event.target.value});
+            break;
+      case 3: this.setState({opt2: event.target.value});
+            break;
+      case 4: this.setState({min: event.target.value});
+            break;
+    }
   }
 
   handleSubmit(event) {
-    alert('A bet was submitted! ' + this.state.value);
+    // parse in default parameters
+    //TO DO
+    alert('A bet was submitted! ' + this.state.value + ", " + this.state.opt1 + ", " + this.state.opt2 + ", " + this.state.min);
     event.preventDefault();
   }
 
@@ -51,18 +67,26 @@ class WriteBet extends React.Component {
         <h class="lead"> Got a bet idea? Submit it here for upvotes! </h>
           <form onSubmit={this.handleSubmit}>
             <div class="form-group">
+            <p></p>
               <label for="BetInput">Bet Proposal:</label>
-              <input type="text" class="form-control" value={this.state.value} placeholder="Enter bet here." onChange={this.handleChange}></input>
+              <input type="text" class="form-control" value={this.state.value} placeholder="Enter bet here (Required)" onChange={(event) => this.handleChange(event, 1)}></input>
             </div>
+            <p></p>
             <div className = "flex-grid">
               <div className = "col">
                 <label>Bet Option 1</label>
-                <input type="text"></input>
+                <input type="text" class="form-control" value={this.state.opt1} placeholder="For (default)" onChange={(event) => this.handleChange(event, 2)}></input>
               </div>   
               <div className = "col">
                 <label>Bet Option 2</label>
-                <input type="text"></input>
+                <input type="text" class="form-control" value={this.state.opt2} placeholder="Against (default)" onChange={(event) => this.handleChange(event, 3)}></input>
               </div>            
+            </div>
+            <div>
+              <p></p>
+              <label> Minimum Bet Amount (Credits) </label>
+              <input type="text" class="form-control" value={this.state.min} placeholder="1 (default)" onChange={(event) => this.handleChange(event, 4)}></input>
+              <p></p>
             </div>
             <button type="submit" class="btn btn-primary">Submit Bet!</button>          
           </form>
